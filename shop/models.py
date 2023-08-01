5740847ad8f399b8a1c3b33b9562397db0865be9
MeOZzzzz <mbagrov1997@gmail.com>
2023-08-01 10:43:16 +0300

    test

--- shop/models.py
+++ shop/models.py
@@ -11,12 +11,21 @@ class Product(models.Model):
     image_url = models.URLField(blank=True, null=True)
     note = models.TextField(blank=True, null=True)
 
+    class Meta:
+        ordering = ['pk']
+    def __str__(self):
+        return f'{self.name} - {self.price}'
+
 class Payment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=20, decimal_places=2, blank=True, null=True)
     time = models.DateTimeField(auto_now_add=True)
     comment = models.TextField(blank=True, null=True)
 
+    class Meta:
+        ordering = ['pk']
+    def __str__(self):
+        return f'{self.user} - {self.amount}'
 class Order(models.Model):
     STATUS_CART = '1_cart'
     STATUS_WAITING_FOR_PAYMENT = '2_waiting_for_payment'
@@ -34,6 +43,22 @@ class Order(models.Model):
     payment = models.ForeignKey(Payment, on_delete=models.PROTECT, blank=True, null=True)
     comment = models.TextField(blank=True, null=True)
 
+    class Meta:
+        ordering = ['pk']
+    def __str__(self):
+        return f'{self.user} - {self.amount} -{self.status}'
+
+    @staticmethod
+    def get_card(user: User):
+        cart = Order.objects.filter(user=user,
+                                    status=Order.STATUS_CART
+                                    ).first()
+        if not cart:
+            cart =Order.objects.create(user=user,
+                                        status=Order.STATUS_CART,
+                                        amount=0)
+        return cart
+
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.PROTECT)
@@ -41,3 +66,7 @@ class OrderItem(models.Model):
     price = models.DecimalField(max_digits=20, decimal_places=2)
     discount = models.DecimalField(max_digits=20, decimal_places=2, default=0)
 
+    class Meta:
+        ordering = ['pk']
+    def __str__(self):
+        return f'{self.product} - {self.price}'
--- shop/tests.py
+++ shop/tests.py
@@ -1,3 +1,46 @@
-from django.test import TestCase
+import zoneinfo
+from datetime import timezone
 
-# Create your tests here.
+from django.test import TestCase
+from shop.models import *
+
+class TestDataBase(TestCase):
+    fixtures = [
+        "shop/fixtures/data.json"
+    ]
+
+    def setUp(self):
+        self.user = User.objects.get(username='root')
+
+    def test_user_exists(self):
+        users = User.objects.all()
+        users_number = users.count()
+        user = users.first()
+        self.assertEqual(users_number, 1)
+        self.assertEqual(user.username, 'root')
+        self.assertTrue(user.is_superuser)
+
+    def test_user_check_password(self):
+        self.assertTrue(self.user.check_password('B@ttelf1lde'))
+
+    def find_cart_number(self):
+        cart_number = Order.objects.filter(user=self.user,
+                                           status=Order.STATUS_CART
+                                           ).count()
+        return cart_number
+    def test_function_get_cart(self):
+
+        self.assertEqual(self.find_cart_number(),0)
+        Order.get_card(self.user)
+
+        self.assertEqual(self.find_cart_number(), 1)
+        Order.get_card(self.user)
+
+        self.assertEqual(self.find_cart_number(), 1)
+
+    def test_cart_older_7days(self):
+        cart = Order.get_card(self.user)
+        cart.creation_time = timezone.datetime(2000,1,1,tzinfo=zoneinfo.ZoneInfo('UTC'))
+        cart.save()
+        cart = Order.get_card(self.user)
+        self.assertEqual((timezone.now()-cart.creation_time).days,0)
